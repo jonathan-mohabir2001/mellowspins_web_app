c8304bc983fd57d99409f3d41359cc9835a648f2
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-06-03 10:08:35 -0400

    Front end updates - bootstrap packages installed

--- client/src/App.js
+++ client/src/App.js
@@ -10,18 +10,14 @@ import HomePage from './components/HomePage';
 import ContactPage from './components/ContactPage';
 import MixesPage from './components/MixesPage';
 import Navbar from './components/Navbar';
-import MixPlayer from './components/MixPlayer';
-import Footer from './components/Footer';
+
 // 6 Pages now succesfully imported
 
 function App() {
   return (
     // PARENT DIV here with ClassName of App (This className may need to change for personal styling)
     <div className="App">
-      
       <Navbar />
-
-
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/mixes" element={<MixesPage />}></Route>
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,13 +1,12 @@
 // Main Home Page to display  information about mellowspins and what this site is
-import React from 'react'
-
+import React from 'react';
 
 function HomePage() {
   return (
     <div>
-        <h1>User Has Landed on Home </h1>
+      <h1>User Has Landed on Home </h1>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
