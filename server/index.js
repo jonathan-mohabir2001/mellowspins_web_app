63535aab5663e03884bbc8600b0f6b352fa95c59
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-06-04 18:01:49 -0400

    BACKEND UPDATE - Google Cloud Storage Incorporated into Web Application

--- server/index.js
+++ server/index.js
@@ -9,6 +9,26 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 // constants set, may need to change the ports, will return after development process
 
+//GOOGLE CLOUD STORAGE code below.
+const { Storage } = require('@google-cloud/storage'); // import library
+
+const storage = new Storage({
+  projectId: 'mellowspins',
+  keyFilename: 'service-account.json',
+});
+// Create Storage Object passed the ID of project ID, and then the fileName of the service account info
+
+async function listFiles() {
+  const [files] = await storage.bucket('mellowspins_mixes_bucket').getFiles();
+  console.log('Files:');
+  files.forEach((file) => {
+    console.log(file.name);
+  });
+}
+// A synch function to get files from the Bucket
+
+listFiles();
+// calling the list files function to be displayed into the terminal.
 app.get('/', (req, res) => {
   res.send('User has entered the Backend of the best web player');
 });
