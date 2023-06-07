ccac6c2dd7006457ca83e96e51e743c8218d39d0
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-06-07 18:50:25 -0400

    FRONT END AND BACKEND UPDATES - React Player incorporated into MixesPage, Backend now using CORS middleware to allow for the sharing of resources from backend to front end

--- client/src/components/MixesPage.js
+++ client/src/components/MixesPage.js
@@ -1,11 +1,29 @@
-// MOST IMPORTANT PAGE OF OVERALL APPLICATION
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ReactPlayer from 'react-player';
 
 function MixesPage() {
+  const [mixes, setMixes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/mixes')
+      .then((response) => {
+        setMixes(response.data);
+      })
+      .catch((error) => {
+        console.error('There was an error!', error);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>User Has Landed on mixes page </h1>
+      <h1>Mixes</h1>
+      {mixes.map((mix) => (
+        <div key={mix}>
+          <ReactPlayer url={mix} controls={true} />
+        </div>
+      ))}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,19 +1,12 @@
-/*
-MELLOWSPINS.CA Node.js Express.js Backend. 
-
-This file is the index file which is the entry point for my 
-backend for this web application. 
-*/
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8000;
-// constants set, may need to change the ports, will return after development process
 
 const mixesRoutes = require('./routes/mixes');
-// IMPORT the created Route Mixes, this is now seperated into its own concern in the Routes Directory
 
+app.use(cors()); // Use cors middleware here
 app.use('/mixes', mixesRoutes);
-// at the route "mixes" use the
 
 app.get('/', (req, res) => {
   res.send('User has entered the Backend of the best web player');
