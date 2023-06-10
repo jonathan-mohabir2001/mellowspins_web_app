f18ecfeee499067bde70a02d8c0309f1ded41272
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-06-10 15:36:16 -0400

    BACKEND UPDATE - Fixed Routing Code which now succesfully can send AUDIO files of each mix to the client when requesting to press PLAY

--- server/routes/mixes.js
+++ server/routes/mixes.js
@@ -16,9 +16,36 @@ const storage = new Storage({
 });
 
 router.get('/', async (req, res) => {
-  const [files] = await storage.bucket('mellowspins_mixes_bucket').getFiles();
-  const fileNames = files.map((file) => file.name);
-  res.json(fileNames);
+  try {
+    const [files] = await storage.bucket('mellowspins_mixes_bucket').getFiles();
+    const fileNames = files.map((file) => file.name);
+
+    // Generate signed URLs for each file
+    const signedUrls = await Promise.all(
+      fileNames.map(async (fileName) => {
+        const file = storage.bucket('mellowspins_mixes_bucket').file(fileName);
+        try {
+          const [url] = await file.getSignedUrl({
+            version: 'v4',
+            action: 'read',
+            expires: Date.now() + 1000 * 60 * 60, // 1 hour
+          });
+          return url;
+        } catch (err) {
+          console.error(`Failed to create signed URL for ${fileName}: ${err}`);
+          return null; // or some error value
+        }
+      })
+    );
+
+    // Filter out any null values (failed to generate signed URL)
+    const validUrls = signedUrls.filter((url) => url !== null);
+
+    res.json(validUrls);
+  } catch (err) {
+    console.error(`Failed to list files: ${err}`);
+    res.status(500).send('Failed to list files');
+  }
 });
 
 module.exports = router;
